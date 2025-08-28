6f2d647d17ad5aabaf57e156dda6b56587f3943b
Nevin1901 <nevinpuri1901@gmail.com>
2025-08-28 21:13:53 +0200

    unprotected

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -14,6 +14,9 @@ export async function AuthButton() {
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.email}!
+      <Button asChild size="sm" variant={"default"}>
+        <Link href="/dashboard">Go to App</Link>
+      </Button>
       <LogoutButton />
     </div>
   ) : (
